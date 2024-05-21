c264ab33b2d16eff17ec16423addcbc9104c1d07
atuhon <kinnjiroa@gmail.com>
2024-05-22 01:36:28 +0900

    21日分

---
+++ top/js/swiper.js
@@ -0,0 +1,20 @@
+
+const swiper = new Swiper('.swiper-container', {
+
+    loop: true,
+    speed:2000,
+    // spaceBetween:24,
+    centeredSlides: true,
+    autoplay: {
+        delay: 1000,
+    },
+    // pagination: {
+    //  el: ".swiper-pagination",
+    //  clickable: true,
+    // },
+    // navigation: {
+    // nextEl: ".swiper-button-next",
+    //  prevEl: ".swiper-button-prev",
+    // },
+
+  });
